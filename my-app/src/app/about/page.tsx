39e2b7b7f96d9dc940f3129fb2ba3e06b62ffaad
Feng Yan <fengyan06080121@gmail.com>
2024-09-26 14:11:11 -0400

    v0' ' ''

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -7,9 +7,9 @@ const AboutPage = () => {
     <div className="min-h-screen flex flex-col justify-between p-8">
       <div>
         <h1 className="text-5xl font-bold mb-4">About Yán<span className="text-sm">(last name)</span> Fēng<span className="text-sm">(first name)</span> 颜丰</h1>
-        <p className="text-lg">welcome! I appreciate you taking the time to learn about me.</p>
+        <p className="text-lg">welcome! I appreciate you taking the time to get to know me.</p>
         <p> - </p>
-        <p className="text-lg">I'm a builder and I care about building products that shape us into better humans.</p>
+        <p className="text-lg">I'm a builder and I care about building deeply humanizing products that shape us into better humans.</p>
         <p> - </p>
         <p className="text-lg">currently, I am studying computer science at Duke University. </p>
         <p> - </p>
