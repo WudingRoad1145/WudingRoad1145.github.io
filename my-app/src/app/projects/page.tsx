7e601b04a454fa943cfc51cdd66f18a28c5f7792
Feng Yan <fengyan06080121@gmail.com>
2024-12-08 11:47:11 -0500

    construction note

--- my-app/src/app/projects/page.tsx
+++ my-app/src/app/projects/page.tsx
@@ -25,12 +25,13 @@ const ProjectsPage = () => {
         <ul className="list-disc ml-8 space-y-4">
           <li><Link href="/projects/hipo" className="text-black hover:underline">HiPo</Link></li>
           <li><Link href="https://rabbit.tech/" className="text-black hover:underline">rabbit r1</Link></li>
-          <li><Link href="/projects/because-it-is-there" className="text-black hover:underline">because it is there - a personal story</Link></li>
           <li><Link href="/projects/multi-agent" className="text-black hover:underline">Multi-agent Collaboration Research</Link></li>
           <li><Link href="/projects/AA" className="text-black hover:underline">Account Abstraction Wallets</Link></li>
           <li><Link href="/projects/wiser" className="text-black hover:underline">The Wiser Project - Summer 2021</Link></li>
           <li><Link href="/projects/big-woman" className="text-black hover:underline">Big Woman</Link></li>
           <li><Link href="/projects/miscellaneous" className="text-black hover:underline">Miscellaneous 无用之用</Link></li>
+          <br></br>
+          <li><Link href="/projects/because-it-is-there" className="text-black hover:underline">[🚧] because it is there - a personal story</Link></li>
         </ul>
       </div>
     </div>
