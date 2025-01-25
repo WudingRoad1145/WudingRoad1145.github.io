2f7c361b3408f2624fffeb4fe8ab5a62cf6f6fa3
Feng Yan <fengyan06080121@gmail.com>
2025-01-24 16:11:13 -0800

    kickstart mtn project

--- my-app/src/app/projects/because-it-is-there/page.tsx
+++ my-app/src/app/projects/because-it-is-there/page.tsx
@@ -5,6 +5,9 @@ export default function ProjectPage() {
     <div className="min-h-screen p-8">
       <h1 className="text-6xl mb-8">because it is there - a personal story</h1>
       <p> Under Construction... </p>
+      <p> “Why do you want to climb Mount Everest?” </p>
+      <p> “Because it's there.” </p>
+      <p> -- George Herbert Leigh Mallory </p>
       {/* Project content */}
     </div>
   );
